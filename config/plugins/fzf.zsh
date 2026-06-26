8292d076b521b7e85e8bfe08c29e10491ffd7719
0oAstro <79555780+0oAstro@users.noreply.github.com>
2026-06-27 04:23:14 +0530

    fix: source available fzf key bindings

--- config/plugins/fzf.zsh
+++ config/plugins/fzf.zsh
@@ -11,7 +11,14 @@ export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
 export FZF_CTRL_T_COMMAND=$FZF_DEFAULT_COMMAND
 
 if [[ -t 0 && -t 1 && -z ${ZSH_EXECUTION_STRING:-} ]]; then
-  source "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
+  local key_bindings
+  for key_bindings in \
+    "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh" \
+    /usr/share/fzf/key-bindings.zsh \
+    /usr/share/doc/fzf/examples/key-bindings.zsh; do
+    [[ -r $key_bindings ]] && { source "$key_bindings"; break; }
+  done
+  unset key_bindings
 fi
 
 unset preview_file preview_dir
